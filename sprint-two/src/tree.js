2cf4e10ca031009033a1121536675860e54c02fa
jawad04 <muhammadijawad10@gmail.com>
2018-09-21 16:02:13 -0700

    Added implementation and time complexity to tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,34 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; 
+  _.extend(newTree,treeMethods); 
 
   return newTree;
 };
 
 var treeMethods = {};
 
+//Time complexity  O(1)
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
+//Time complexity O(n)
 treeMethods.contains = function(target) {
+  var node = this.value;
+  if(node === target){
+    return true;
+  }
+  if(this.children.length > 0){
+    for(var i = 0; i < this.children.length; i++){
+      if(this.children[i].contains(target)){
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
