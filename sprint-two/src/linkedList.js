8e228fe70a9cb54d1f1eec7beccef8e58ccc3c70
jawad04 <muhammadijawad10@gmail.com>
2018-09-20 18:17:13 -0700

    Finished LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,33 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  //O(1) time complexity
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (this.tail) {
+     this.tail.next = newNode; 
+    } else if(!this.tail) {
+      this.head = newNode;//4
+    }
+    this.tail = newNode;
+    
   };
 
   list.removeHead = function() {
+    var currentHead = this.head.value;
+    this.head = this.head.next;
+    return currentHead;
   };
 
   list.contains = function(target) {
+    var compareEl = this.head;
+    while(compareEl !== null){
+      if(compareEl.value === target){
+         return true;
+      }
+    compareEl = compareEl.next;
+    }
+    return false;
   };
 
   return list;
