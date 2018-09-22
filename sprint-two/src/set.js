b2053408e0e813acc217182f3d25a7a7670e5ff5
jawad04 <muhammadijawad10@gmail.com>
2018-09-21 18:41:01 -0700

    Added implementation and time complexity to set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
+  
   return set;
 };
 
 var setPrototype = {};
 
+
+//Time complexity O(n)
 setPrototype.add = function(item) {
+  if(!this._storage.includes(item)){
+    this._storage.push(item);
+  }  
 };
 
+
+//Time complexity O(n)
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
-setPrototype.remove = function(item) {
+//Time complexity O(n)
+setPrototype.remove = function(item) {//2
+  var itemIndex = this._storage.indexOf(item);//2
+  if(itemIndex !== -1){
+    this._storage.splice(itemIndex,1);
+  }
 };
 
 /*
