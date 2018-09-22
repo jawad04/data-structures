38d3bb5a49e026af024d245b40e21e3253a5cbc8
jawad04 <muhammadijawad10@gmail.com>
2018-09-22 15:52:33 -0700

    Added implementation to binary search tree and added time complexity.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,59 @@
 var BinarySearchTree = function(value) {
+  var obj = {};
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  _.extend(obj, binaryMethods)
+  return obj;
 };
+//Time complexity O(n)
+var binaryMethods = {};
+binaryMethods.insert = function(value){
+  var root = BinarySearchTree(value);
+  recurseFunc(this);
+  function recurseFunc(node){
+    if(value < node.value){
+      if(node.left === null ){
+        node.left = root;
+      }else{
+        recurseFunc(node.left);
+      }
+    }
+    if(value > node.value){
+      if(node.right === null){
+        node.right = root;
+      }else{
+        recurseFunc(node.right);
+      }
+    }
+  }
+};
+//Time complexity O(n)
+binaryMethods.contains = function(value){
+  if(value > this.value && this.right !== null){
+    return this.right.contains(value);
+  }else if(value < this.value && this.left !== null){
+    return this.left.contains(value);
+  }else if(this.value === value){
+    return true;
+  }else{
+    return false;
+  }
+};
+
+//Time complexity O(n)
+binaryMethods.depthFirstLog = function(cb,node){
+ if(node === undefined){
+   node = this;
+ }else if(node === null){
+    return;
+ }
+ cb(node.value);
+ node.depthFirstLog(cb,node.left);
+ node.depthFirstLog(cb, node.right);
+
+};
+
 
 
 /*
