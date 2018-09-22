d3c3843a45b4a49a0b6aaf39726a2076221e594e
jawad04 <muhammadijawad10@gmail.com>
2018-09-22 09:53:16 -0700

    Added implementation to hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,50 @@
 
 
 var HashTable = function() {
-  this._limit = 8;
+  this._limit = 8;//number of buckets in array
   this._storage = LimitedArray(this._limit);
+  
 };
-
+//Time complexity O(n)
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  var bucket = this._storage.get(index);
+  var tuple = [k, v];
 
+  if(bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+      } else {
+        bucket.push(tuple);
+      }
+    }
+  } else {
+      this._storage.set(index, [tuple]);
+  }
+  
+  
+};
+//Time complexity O(n)
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
+//Time complexity O(n)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i++){
+    if(bucket[i][0] === k){
+      bucket[i][0] = undefined;
+    }
+  }
 };
 
 
